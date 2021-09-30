3e0a71df2c643a4da3dbb3cc7a9ed75e5e4dbed7
batuanr <tuanpesi@gmail.com>
2021-09-30 14:44:30 +0700

    xong

---
+++ src/MyLinkedListQueue.java
@@ -0,0 +1,31 @@
+public class MyLinkedListQueue {
+    private Node head;
+    private Node tail;
+    class Node {
+        public Node next;
+        public int key;
+
+        public Node(int key) {
+            this.key = key;
+            this.next = null;
+        }
+    }
+    public void enqueue(int key) {
+        Node temp = new Node(key);
+        if (this.tail == null) {
+            this.head = this.tail = temp;
+            return;
+        }
+        this.tail.next = temp;
+        this.tail = temp;
+    }
+    public Node dequeue() {
+        if (this.head == null)
+            return null;
+        Node temp = this.head;
+        this.head = this.head.next;
+        if (this.head == null)
+            this.tail = null;
+        return temp;
+    }
+}
